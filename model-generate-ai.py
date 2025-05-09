8c88c59fbb5881afe7f906b046b67fed9b3ba499
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2025-05-09 20:27:42 +0700

    update new

---
+++ model-generate-ai.py
@@ -0,0 +1,63 @@
+import random
+import numpy as np
+import pandas as pd
+from nltk.tokenize import RegexpTokenizer
+
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import LSTM, Dense, Activation
+from tensorflow.keras.optimizers import RMSprop
+text_df = pd.read_csv("./Dataset_NCKH/fake_or_real_news.csv")
+text = list(text_df.text.values)
+jointed_text = " ".join(text)
+partial_text = jointed_text[:10000]
+tokenizer = RegexpTokenizer(r"\w+")
+tokens = tokenizer.tokenize(partial_text.lower())
+unique_tokens = np.unique(tokens)
+unique_token_index = {token: i for i, token in enumerate(unique_tokens)}
+unique_token_index
+n_words = 10
+input_words = []
+next_words = []
+
+for i in range(len(tokens) - n_words):
+    input_words.append(tokens[i:i+n_words])
+    next_words.append(tokens[i+n_words])
+X=np.zeros((len(input_words), n_words, len(unique_tokens)), dtype=bool)
+y=np.zeros((len(next_words),len(unique_tokens)), dtype=bool)
+for i, words in enumerate(input_words):
+    for j, word in enumerate(words):
+        X[i,j, unique_token_index[word]] = 1
+    y[i, unique_token_index[next_words[i]]] = 1
+model = Sequential()
+model.add(LSTM(128, input_shape=(n_words, len(unique_tokens)), return_sequences=True))
+model.add(LSTM(128))
+model.add(Dense(len(unique_tokens)))
+model.add(Activation('softmax'))
+model.compile(loss="categorical_crossentropy", optimizer=RMSprop(learning_rate=0.01), metrics=["accuracy"])
+model.fit(X,y, batch_size=128, epochs=10, shuffle=True)
+model.save("mymodel.h5")
+model = load_model("mymodel.h5")
+def predict_next_word(input_text, n_best):
+    input_text = input_text.lower()
+    X = np.zeros((1, n_words, len(unique_tokens)))
+    for i, word in enumerate(input_text.split()):
+        X[0, i, unique_token_index[word]] = 1
+
+    predictions = model.predict(X)[0]
+    return np.argpartition(predictions, -n_best)[-n_best:]
+possible = predict_next_word("He will have to look into this thing and he", 5)
+print([unique_tokens[idx] for idx in possible])
+def generate_text(input_text, text_length ,creativity=3):
+    word_sequence = input_text.split()
+    current=0
+    for _ in range(text_length):
+        sub_sequence = " ".join(tokenizer.tokenize(" ".join(word_sequence).lower())[current:current+n_words])
+        try:
+            choice = unique_tokens[random.choice(predict_next_word(sub_sequence, creativity))]
+        except:
+            choice = random.choice(unique_tokens)
+        word_sequence.append(choice)
+        current += 1
+    return " ".join(word_sequence)
+generate_text("He will have to look into this thing and he", 100, 5)
+
